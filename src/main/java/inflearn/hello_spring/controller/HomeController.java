659c567d0967608cc6e5b812e9eacaaca08a3a5f
beckhem96 <beckhem96@naver.com>
2024-11-14 01:39:26 +0900

    mvc 회원 등록, 조회

---
+++ src/main/java/inflearn/hello_spring/controller/HomeController.java
@@ -0,0 +1,15 @@
+package inflearn.hello_spring.controller;
+
+import inflearn.hello_spring.domain.Member;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
