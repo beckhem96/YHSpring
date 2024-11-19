6c72c5cc7527f79330aac535d9f586a4f6a29435
beckhem96 <beckhem96@naver.com>
2024-11-20 00:10:34 +0900

    jdbc Practice

--- src/main/java/inflearn/hello_spring/SpringConfig.java
+++ src/main/java/inflearn/hello_spring/SpringConfig.java
@@ -1,13 +1,23 @@
 package inflearn.hello_spring;
 
+import inflearn.hello_spring.repository.JdbcTemplateMemberRepository;
 import inflearn.hello_spring.repository.MemberRepository;
-import inflearn.hello_spring.repository.MemoryMemberRepository;
 import inflearn.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
---
+++ src/test/java/inflearn/hello_spring/service/MemberServiceJdbcTest.java
@@ -0,0 +1,56 @@
+package inflearn.hello_spring.service;
+
+import inflearn.hello_spring.domain.Member;
+import inflearn.hello_spring.repository.MemberRepository;
+import inflearn.hello_spring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceJdbcTest {
+
+    @Autowired MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("LWT1");
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("LWT1");
+        Member member2 = new Member();
+        member2.setName("LWT1");
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2)); // exception type check
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다."); // error message check
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        } catch (IllegalStateException e) {
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        }
+        // then
+    }
+}
